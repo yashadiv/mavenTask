568ad51a92ed4dcd1d4991a1409b5ee44ae86a0b
yashadiv <yashadiv@mail.ru>
2024-11-08 16:51:20 +0300

    Выполнение задания по Maven

---
+++ GeometryLibrary/GeometryUtils/src/main/java/figures/Trinagle.java
@@ -0,0 +1,22 @@
+package figures;
+
+public class Trinagle {
+    private double a;
+    private double b;
+    private double c;
+
+    public double perimetr(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        return (a+b+c);
+    }
+
+    public double square (double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        Trinagle trinagle = new Trinagle();
+        double p = trinagle.perimetr(a,b,c);
+        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    }
+}
